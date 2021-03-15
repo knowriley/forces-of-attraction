fe297b85439d00d852afc34314af27287da12c25
Riley <knowles.riley@gmail.com>
2021-03-14 20:06:08 -0700

    setup skeleton, runs on local server no errors

---
+++ js/matrix.js
@@ -0,0 +1,32 @@
+class Matrix {
+
+    constructor(_config, _data) {
+      this.config = {
+        parentElement: _config.parentElement,
+        containerWidth: 500,
+        containerHeight: 350,
+        margin: {top: 15, right: 15, bottom: 20, left: 25}
+      }
+      this.data = _data;
+      this.initVis();
+    }
+
+    initVis() {
+        let vis = this;
+        console.log('init matrix');
+        // TODO
+    }
+
+    updateVis() {
+        let vis = this;
+        // TODO
+
+        vis.renderVis();
+    }
+
+    renderVis() {
+        let vis = this;
+        // TODO
+    }
+
+}
