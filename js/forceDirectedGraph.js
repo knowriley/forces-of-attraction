055405b47d2d11899b811fa4f137a4db5b359515
rowdl22 <rowdl22@students.cs.ubc.ca>
2021-03-30 16:02:52 -0700

    draggable nodes

--- js/forceDirectedGraph.js
+++ js/forceDirectedGraph.js
@@ -71,6 +71,7 @@ class ForceDirectedGraph extends View {
             .attr('r', 4)
             .attr('fill', d => vis.colorScale(decode(vis.attribute)(d)))
             .attr('stroke', 'black')
+            .call(drag(vis.graph))
         .on('mouseover', (e, d) => {
             d3.select('#tooltip')
                 .style('display', 'block')
@@ -92,5 +93,31 @@ class ForceDirectedGraph extends View {
                 .attr('cy', d => d.y);
             });
     }
+}
 
+// Drag simulation code curtesy of
+// https://observablehq.com/@d3/force-directed-graph
+const drag = simulation => {
+  
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+    
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+    
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+    
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
 }
